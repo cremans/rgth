8340fdcf4c288c3358dd8d23890ef05759f62508
cremans <88071871+cremans@users.noreply.github.com>
2021-08-24 11:10:13 +0000

    Update start.bat

--- start.bat
+++ start.bat
@@ -12,7 +12,6 @@ sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
 ICACLS C:\Windows\Temp /grant administrator:F >nul
 ICACLS C:\Windows\installer /grant administrator:F >nul
-echo Berhasil Menginstal!, Jika RDPnya Mati Silahkan Rebuild Lagi Ok!
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Tidak bisa mendapatkan NGROK tunnel, pastikan NGROK_AUTH_TOKEN benar di Settings> Secrets> Repository secret. Mungkin VM Anda sebelumnya masih berjalan: https://dashboard.ngrok.com/status/tunnels "
 echo Username: administrator
